22a03d9c4de9fb5aad6846c7b2f470d53fa1eeb3
thinksource <foretribe@gmail.com>
2016-10-18 08:50:40 +1100

    add person

--- src/main/java/com/lb/web/controller/WebController.java
+++ src/main/java/com/lb/web/controller/WebController.java
@@ -1,13 +1,18 @@
 package com.lb.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.ui.Model;
+import com.lb.datajpa.user.service.UserService;
 @RestController
+@RequestMapping("/")
 public class WebController {
 
-	@GetMapping("/person")
-	public String hello() {
+	@RequestMapping("/person")
+	public String listAll(Model model) {
+		UserService us=new UserService();
+		model.addAttribute("presonal", us.getAllUsers());
 		return "personal";
 	}
 
